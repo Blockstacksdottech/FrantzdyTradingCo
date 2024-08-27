7d1e7fce980e62f4d9e1e80d7be08b1978d55c7b
Blockstacks Technologies <99893162+Blockstacksdottech@users.noreply.github.com>
2024-08-27 23:53:26 +0530

    Update editblog.js

--- pages/panel/editblog.js
+++ pages/panel/editblog.js
@@ -4,66 +4,62 @@ import Sidebar from "../components/panel/sidebar";
 import Menu from "../components/panel/menu";
 import ScriptLink from "../components/panel/scriptlink";
 import Footer from "../components/panel/footer";
-import React, { useEffect,useState,useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Checker from "../components/Checker";
 import SummernoteLite from "react-summernote-lite";
 
-import 'react-summernote-lite/dist/esm/dist/summernote-lite.min.css';
+import "react-summernote-lite/dist/esm/dist/summernote-lite.min.css";
 import { toast } from "react-toastify";
-import { uploadPatchFiles,req,formatImage } from "@/helpers";
+import { uploadPatchFiles, req, formatImage } from "@/helpers";
 import { useRouter } from "next/router";
 import { useSearchParams } from "next/navigation";
 
-
 const CreateBlog = () => {
-
   const [imageFiles, setImageFiles] = useState([]);
-  const [image,setImage] = useState(null);
-  const [title,setTitle] = useState("");
-  const [loading,setLoading] = useState(true)
-  const [article,setArticle] = useState({})
-  const [content,setContent] = useState("")
+  const [image, setImage] = useState(null);
+  const [title, setTitle] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [article, setArticle] = useState({});
+  const [content, setContent] = useState("");
 
   const noteRef = useRef();
 
-  const router = useRouter()
-  const params = useSearchParams()
-  const id = params.get("id")
+  const router = useRouter();
+  const params = useSearchParams();
+  const id = params.get("id");
+
+  // useEffect(() => {
+  //   const script = document.createElement("script");
+  //   script.src = "../panel/js/datatable.js";
+  //   script.async = true;
+  //   document.body.appendChild(script);
+  // }, []);
 
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "../panel/js/datatable.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
-
-  useEffect(() => {
-    if (id){
-      fetchArticle(id)
+    if (id) {
+      fetchArticle(id);
     }
-  },[id])
+  }, [id]);
 
   const fetchArticle = async (id) => {
-    const resp = await req(`blog/${id}/`)
-    if (resp){
-      setArticle(resp)
-      setTitle(resp.title)
-      setContent(resp.content)
-      setLoading(false)
-
+    const resp = await req(`blog/${id}/`);
+    if (resp) {
+      setArticle(resp);
+      setTitle(resp.title);
+      setContent(resp.content);
+      setLoading(false);
     }
-  }
-
+  };
 
   const save = async () => {
-    if (!image){
-      toast.error("Please select an image")
-    }else{
+    if (!image) {
+      toast.error("Please select an image");
+    } else {
       const code = noteRef.current.summernote("code");
       const body = {
         title,
-        content : code
-      }
+        content: code,
+      };
       const res = await uploadPatchFiles([image], body, "image", `blog/${id}/`);
       if (res) {
         toast.success("updated");
@@ -72,10 +68,7 @@ const CreateBlog = () => {
         toast.error("failed upload");
       }
     }
-    
-    
-  }
-
+  };
 
   return (
     <>
@@ -108,86 +101,101 @@ const CreateBlog = () => {
           </div>
         </section>
         <Checker only_admin={true}>
-          {
-            !loading && <section className="content">
-            <div className="container-fluid">
-              <div className="row">
-                <div className="col-12">
-                  <div className="card">
-                    <div className="card-body">
-                      <div className="form-group">
-                        <label>Topic</label>
-                        <input
-                          className="form-control form-control-lg"
-                          type="text"
-                          value={title}
-                          onChange={(e) => setTitle(e.target.value)}
-                        />
-                      </div>
-                      <div className="form-group">
-                        <label>Thumbnail</label>
-                        <input
-                          className="form-control form-control-lg"
-                          type="file"
-                          onChange={(e) => setImage(e.target.files[0])}
-                        />
-                      </div>
-                      {
-                        article.image && <img src={formatImage(article.image)} style={{width:200,height:200}} />
-                      }
-                      <div className="form-group">
-                        <label>Description</label>
-                        {/* <textarea id="summernote"></textarea> */}
-                        <SummernoteLite
-      ref={noteRef}
-      defaultCodeValue={content}
-      placeholder={"Write something here..."}
-      tabsize={2}
-      lang="zh-CN" // only if you want to change the default language
-      dialogsInBody={true}
-      blockquoteBreakingLevel={0}
-      toolbar={[
-        ['style', ['style']],
-        ['font', ['bold', 'underline', 'clear', 'strikethrough', 'superscript', 'subscript']],
-        ['fontsize', ['fontsize']],
-        ['fontname', ['fontname']],
-        ['color', ['color']],
-        ['para', ['ul', 'ol', 'paragraph']],
-        ['table', ['table']],
-        ['insert', ['link', 'picture', 'video', 'hr']],
-        ['view', ['fullscreen', 'codeview', 'help']]
-      ]}
-      fontNames={[
-        "Arial",
-        "Georgia",
-        "Verdana",
-        "e.t.c..."
-      ]}
-      callbacks={{
-        onImageUpload: function (files){
-            setImageFiles(files);
-        },
-        onKeyup: function (e){},
-        onKeyDown: function (e){},
-        onPaste: function (e){}
-      }}
-    />
-                      </div>
-                      <div className="form-group">
-                        <div className="float-right">
-                          <a className="btn btn-table-dark box-shadow-2" onClick={save}>
-                            Save
-                          </a>
+          {!loading && (
+            <section className="content">
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-12">
+                    <div className="card">
+                      <div className="card-body">
+                        <div className="form-group">
+                          <label>Topic</label>
+                          <input
+                            className="form-control form-control-lg"
+                            type="text"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                          />
+                        </div>
+                        <div className="form-group">
+                          <label>Thumbnail</label>
+                          <input
+                            className="form-control form-control-lg"
+                            type="file"
+                            onChange={(e) => setImage(e.target.files[0])}
+                          />
+                        </div>
+                        {article.image && (
+                          <img
+                            src={formatImage(article.image)}
+                            style={{ width: 200, height: 200 }}
+                          />
+                        )}
+                        <div className="form-group">
+                          <label>Description</label>
+                          {/* <textarea id="summernote"></textarea> */}
+                          <SummernoteLite
+                            ref={noteRef}
+                            defaultCodeValue={content}
+                            placeholder={"Write something here..."}
+                            tabsize={2}
+                            lang="zh-CN" // only if you want to change the default language
+                            dialogsInBody={true}
+                            blockquoteBreakingLevel={0}
+                            toolbar={[
+                              ["style", ["style"]],
+                              [
+                                "font",
+                                [
+                                  "bold",
+                                  "underline",
+                                  "clear",
+                                  "strikethrough",
+                                  "superscript",
+                                  "subscript",
+                                ],
+                              ],
+                              ["fontsize", ["fontsize"]],
+                              ["fontname", ["fontname"]],
+                              ["color", ["color"]],
+                              ["para", ["ul", "ol", "paragraph"]],
+                              ["table", ["table"]],
+                              ["insert", ["link", "picture", "video", "hr"]],
+                              ["view", ["fullscreen", "codeview", "help"]],
+                            ]}
+                            fontNames={[
+                              "Arial",
+                              "Georgia",
+                              "Verdana",
+                              "e.t.c...",
+                            ]}
+                            callbacks={{
+                              onImageUpload: function (files) {
+                                setImageFiles(files);
+                              },
+                              onKeyup: function (e) {},
+                              onKeyDown: function (e) {},
+                              onPaste: function (e) {},
+                            }}
+                          />
+                        </div>
+                        <div className="form-group">
+                          <div className="float-right">
+                            <a
+                              className="btn btn-table-dark box-shadow-2"
+                              onClick={save}
+                            >
+                              Save
+                            </a>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </section>
-          }
-          
+            </section>
+          )}
         </Checker>
       </div>
       <Footer />
