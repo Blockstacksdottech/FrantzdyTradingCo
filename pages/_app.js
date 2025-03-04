8bb34359c7f3c4e3b04ddcac8e476e4fc1eff3c3
vintex <vin@gg.com>
2025-03-04 22:21:38 +0000

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,27 @@
 import { ToastContainer } from "react-toastify";
 import { UserProvider } from "@/contexts/UserContextData";
-
+import { useRef } from 'react';
+import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
 
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
+  const tawkMessengerRef = useRef();
+
+  const handleMinimize = () => {
+        tawkMessengerRef.current.minimize();
+        
+    };
+
   return (
     <>
       <UserProvider>
         <Component {...pageProps} />
       </UserProvider>
+      <TawkMessengerReact
+                propertyId="67c7796c47285a190ee0afd7"
+                widgetId="1ilhj4kjs"
+                ref={tawkMessengerRef}/>
 
       <ToastContainer
         position="top-right"
